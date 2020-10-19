1071ffe636c6cd9bb81787c9f2d6ec3acf98bb2a
Emma Wishart <emma.wishart@twikit.com>
2020-10-19 09:54:12 +0200

    many problems

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,63 +1,113 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import limonImg from "../assets/products/nocco-limon.jpg";
+import miamiImg from "../assets/products/nocco-miami.jpg";
+import peachImg from "../assets/products/nocco-peach.jpg";
+import pearImg from "../assets/products/nocco-pear.jpg";
+import tropicalImg from "../assets/products/nocco-tropical.jpg";
+import {Link} from "react-router-dom";
 
 export default class Products extends Component {
     constructor(props) {
         super(props);
 
-        /*        axios.get("http://localhost:8081/api/products/").then(res => {
-                    this.setState({
-                        data: res.data
-                    });
-                });*/
-
         this.state = {
             data: [],
-            buttonClicked: false
+            // eslint-disable-next-line no-undef
+            imageLocation: `../assets/products/`
         };
-        this.getDetails = this.getDetails.bind(this);
-
-        /*        const product = axios.get('http://localhost:8081/api/products/')
-                    .then(res => console.log('RESPONSE', res.data));
-
-                product.title = product.title.bind(this);
-                product.description = product.description.bind(this);
-                product.brand = product.brand.bind(this);
-                product.price = product.price.bind(this);*/
-
+        let imageLocation = `../assets/products/${this.state.data.image}`;
+        //console.log('imageLocation', imageLocation);
+        if (this.state.data.image) {
+            this.setState({
+                image: imageLocation
+            });
+            console.log('IMG', imageLocation)
+        }
     }
 
+    SubmitValue = () => {
+        this.props.handleData(this.state.data)
+    }
     componentDidMount() {
         axios.get('http://localhost:8081/api/products/')
             .then((res) => {
                 console.log('response', res.data)
+                console.log('img', res.data[0].image)
+                for (let i = 0; i < res.data.length; i++) {
                 this.setState({
-                    data: res.data
+                    data: res.data,
+                    imageLocation: `../assets/products/${res.data[i].image}`
                 })
+                    console.log('imageLocation', this.state.imageLocation)
+            }
             });
+/*        this.setState(
+            this.state.imageLocation.map(data => {
+                imageLocation: `../assets/products/${data.image}`
+            }))*/
+        console.log('this.state.data.image', this.state.imageLocation);
     }
 
-    getDetails() {
-        if (!this.state.buttonClicked) {
-            this.setState({
-                buttonClicked: true
-            });
-        }
-    }
 
     render() {
         return (
             this.state.data.map(data => {
                 return (
                     <React.Fragment>
-                        <p>
-                            {" "}
-                            <b>Title</b> : {data.title}
-                        </p>
-                        <p>
-                            <b>Brand</b> : {data.brand}
-                        </p>
-                        <hr/>
+                        <main className="d-flex">
+                            <div className="d-flex container flex-column flex-sm-row">
+                                <div className="card-deck d-flex col-md-12 ">
+                                    <div className="card py-1 justify-content-around shadow-sm">
+                                        <img className="card-img-top" src={limonImg} alt={data.image}/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{data.title}</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">€{data.price}   |   {data.size}</h6>
+                                            <h6 className="card-subtitle mb-2 text-muted">{data.brand}</h6>
+                                            <p className="card-text"><b>Description</b>: {data.description}</p>
+                                            <Link to="/limon" className="btn btn-primary">{data.title}</Link>
+                                        </div>
+                                        <br/>
+                                    </div>
+                                    <div className="card py-1 justify-content-around shadow-sm">
+                                        <img className="card-img-top" src={miamiImg} alt={data.image}/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{data.title}</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">€{data.price}   |   {data.size}</h6>
+                                            <h6 className="card-subtitle mb-2 text-muted">{data.brand}</h6>
+                                            <p className="card-text"><b>Description</b>: {data.description}</p>
+                                            <Link to="/limon" className="btn btn-primary">{data.title}</Link>
+                                        </div>
+                                        <br/>
+                                    </div>
+                                </div>
+                            </div>
+{/*                            <div className="d-flex container flex-column flex-sm-row">
+                                <div className="flex-column justify-content-around">
+                                    <div className="card-deck">
+                                        <div className="card col-sm-6 col-md-4 col-lg-3 mb-4 mt-4 shadow-sm">
+                                            <img className="card-img-top" src={`${this.state.imageLocation}`} alt={data.image}/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">{data.title}</h5>
+                                                <h6 className="card-subtitle mb-2 text-muted">€{data.price}   |   {data.size}</h6>
+                                                <h6 className="card-subtitle mb-2 text-muted">{data.brand}</h6>
+                                                <p className="card-text"><b>Description</b>: {data.description}</p>
+                                                <Link to="/limon" className="btn btn-primary">{data.title}</Link>
+                                            </div>
+                                            <img className="card-img-top" src={limonImg} alt={data.title}/>
+                                            <ul>
+                                                <li>
+                                                    <b>Ingredients</b>: {data.ingredients}
+                                                </li>
+                                            </ul>
+                                            <hr/>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>*/}
+                        </main>
+
                     </React.Fragment>
                 );
             })
